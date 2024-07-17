09698bf6f936ffeef3a11dc186bf55f75c5c9821
Hakan ÖZER <hakanozer02@gmail.com>
2024-07-17 15:18:06 +0300

    Product Detail

--- overview/src/pages/ProductDetail.tsx
+++ overview/src/pages/ProductDetail.tsx
@@ -1,19 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import { Product } from '../models/IProducts'
+import { singleProduct } from '../services/productService'
 
 function ProductDetail() {
   
+  const [bigImage, setBigImage] = useState('')  
+  const [product, setProduct] = useState<Product>()
   const params = useParams()
   const location = useLocation()
   useEffect(() => {
-    console.log(params.pid)
+    const pid = params.pid
+    if (pid) {
+        singleProduct(pid).then(res => {
+            const data = res.data
+            setProduct(data)
+            setBigImage(data.images[0])
+        })
+    }
   }, [])
   
   const item = location.state as Product
 
   return (
-    <div>ProductDetail</div>
+    <>
+        { product && 
+            <div className='row'>
+                <div className='col-sm-6'>
+                    <h2>{product.title}</h2>
+                    <p>{product.description}</p>
+                    <div>{product.price}₺</div>
+                </div>
+                <div className='col-sm-6'>
+                    <img src={bigImage} className='img-fluid' style={{maxHeight: 400,}} />
+                    <hr></hr>
+                    { product.images.map((item, index) => 
+                        <img onClick={() => setBigImage(item)} key={index} src={item} className='img-thumbnail' style={{maxWidth: 100,}} />
+                    )}
+                </div>
+            </div>
+        }
+    </>
   )
 }
 
--- overview/src/services/productService.ts
+++ overview/src/services/productService.ts
@@ -1,7 +1,11 @@
-import { IProducts } from "../models/IProducts"
+import { IProducts, Product } from "../models/IProducts"
 import { configApi } from "./api"
 
 
 export const allProduct = () => {
     return configApi.get<IProducts>('products')
+}
+
+export const singleProduct = (id: string) => {
+    return configApi.get<Product>('products/'+id)
 }
